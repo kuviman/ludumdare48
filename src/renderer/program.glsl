3acc61a427d79970be2ffd5d5c3fb58db1127899
kuviman <kuviman@gmail.com>
2021-04-24 17:37:59 +0400

    Fix upside down textures

--- src/renderer/program.glsl
+++ src/renderer/program.glsl
@@ -8,7 +8,7 @@ uniform mat4 u_view_matrix;
 uniform mat4 u_model_matrix;
 
 void main() {
-    v_vt = a_pos;
+    v_vt = vec2(a_pos.x, 1.0 - a_pos.y);
     gl_Position = u_projection_matrix * u_view_matrix * u_model_matrix * vec4(a_pos, 0.0, 1.0);
 }
 #endif
